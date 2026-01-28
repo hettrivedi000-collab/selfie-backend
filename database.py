85478f0978d587bfffbc4a951de621edeb19a0da
hettrivedi000-collab <hettrivedi000@gmail.com>
2026-01-28 13:07:56 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
-MONGO_URL = os.getenv("MANGO_URL")
+MONGO_URL = os.getenv("MONGO_URL")
 client = AsyncIOMotorClient(MONGO_URL)
 db = client["selfie_db"]
 collection = db["users"]
-
-
